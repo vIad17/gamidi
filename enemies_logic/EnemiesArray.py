648b86c4678b4c640610e711c386346620efb29b
Vlad <titkov2002@gmail.com>
2026-01-06 11:52:59 +0600

    added more enemies

--- enemies_logic/EnemiesArray.py
+++ enemies_logic/EnemiesArray.py
@@ -1,6 +1,6 @@
 from enemies_logic.FileReader import ReadFromFile
 from typing import List, TypedDict
-from units import Unit
+# from units import Unit
 
 class Enemy(TypedDict):
     image: List[int]
@@ -15,8 +15,33 @@ enemies: List[Enemy] = [
     "color": [255, 0, 0],
   },
   {
-    "name" : "Square",
-    "image": ReadFromFile("square"),
+    "name" : "Ant",
+    "image": ReadFromFile("ant"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Bull",
+    "image": ReadFromFile("bull"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Dolphin",
+    "image": ReadFromFile("dolphin"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Dragon",
+    "image": ReadFromFile("dragon"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Fireflies",
+    "image": ReadFromFile("fireflies"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Flame",
+    "image": ReadFromFile("flame"),
     "color": [255, 153, 0],
   },
   {
@@ -25,8 +50,23 @@ enemies: List[Enemy] = [
     "color": [255, 153, 0],
   },
   {
-    "name" : "Ant",
-    "image": ReadFromFile("ant"),
+    "name" : "Eye",
+    "image": ReadFromFile("eye"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Jellyfish",
+    "image": ReadFromFile("jellyfish"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Snake",
+    "image": ReadFromFile("snake"),
+    "color": [255, 153, 0],
+  },
+  {
+    "name" : "Turtle",
+    "image": ReadFromFile("turtle"),
     "color": [255, 153, 0],
   }
 ]
